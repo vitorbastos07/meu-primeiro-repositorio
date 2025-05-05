77818cd8efcae2e455831aaa64580539a6c9d350
Bibancos <gabrielgbr@al.insper.edu.br>
2025-05-05 15:32:08 -0300

    ex6

--- dev2.py
+++ dev2.py
@@ -1,17 +1,14 @@
-import random
-
-def rolar_dados(n):
-    lista = []
-    for _ in range(n):
-        lista.append(random.randint(1, 6))
-    return lista
+def guardar_dado(dados_rolados, dados_no_estoque, dado_para_guardar):
+    valor = dados_rolados[dado_para_guardar]
+    dados_rolados.pop(dado_para_guardar)
+    dados_no_estoque.append(valor)
+    return [dados_rolados, dados_no_estoque]
 
 def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
-     valor = dados_no_estoque[dado_para_remover]
-     dados_no_estoque.pop(dado_para_remover)
-     dados_rolados.append(valor)
-     return [dados_rolados, dados_no_estoque]
-
+    valor = dados_no_estoque[dado_para_remover]
+    dados_no_estoque.pop(dado_para_remover)
+    dados_rolados.append(valor)
+    return [dados_rolados, dados_no_estoque]
 
 def calcula_pontos_regra_simples(lista):
     pontuacao = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
@@ -20,9 +17,32 @@ def calcula_pontos_regra_simples(lista):
             pontuacao[dado] += dado
     return pontuacao
 
-
 def calcula_pontos_soma(lista):
     total = 0
     for valor in lista:
         total += valor
     return total
+
+def calcula_pontos_sequencia_baixa(lista):
+    lista_ordenada = sorted(lista)
+    contador = 1
+    for i in range(1, len(lista_ordenada)):
+        if lista_ordenada[i] == lista_ordenada[i-1] + 1:
+            contador += 1
+            if contador >= 4:
+                return 15
+        elif lista_ordenada[i] != lista_ordenada[i-1]:
+            contador = 1
+    return 0
+
+def calcula_pontos_sequencia_alta(lista):
+    lista_ordenada = sorted(lista)
+    contador = 1
+    for i in range(1, len(lista_ordenada)):
+        if lista_ordenada[i] == lista_ordenada[i-1] + 1:
+            contador += 1
+            if contador >= 5:
+                return 30
+        elif lista_ordenada[i] != lista_ordenada[i-1]:
+            contador = 1
+    return 0

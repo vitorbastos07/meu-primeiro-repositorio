44362ff4af619a8779b27a16ef7baab151d43740
vitorbastos07 <vitorbb@al.insper.edu.br>
2025-05-05 15:23:41 -0300

    ex3

--- dev1.py
+++ dev1.py
@@ -5,3 +5,17 @@ def rolar_dados(n):
     for _ in range(n):
         lista.append(random.randint(1, 6))
     return lista
+
+def remover_dado(dados_rolados, dados_no_estoque, dado_para_remover):
+     valor = dados_no_estoque[dado_para_remover]
+     dados_no_estoque.pop(dado_para_remover)
+     dados_rolados.append(valor)
+     return [dados_rolados, dados_no_estoque]
+
+
+def calcula_pontos_regra_simples(lista):
+    pontuacao = {1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0}
+    for dado in lista:
+        if dado in pontuacao:
+            pontuacao[dado] += dado
+    return pontuacao

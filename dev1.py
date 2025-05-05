b6187ff05b971eb822bfea4a5874f8ec45323cd9
vitorbastos07 <vitorbb@al.insper.edu.br>
2025-05-05 16:00:56 -0300

    ex 9 correto

--- dev1.py
+++ dev1.py
@@ -73,16 +73,3 @@ def calcula_pontos_quadra(lista):
             contagem[dado] += 1
         else:
             contagem[dado] = 1
-
-def calcula_pontos_quina(lista):
-    contagem = {}
-    for dado in lista:
-        if dado in contagem:
-            contagem[dado] += 1
-        else:
-            contagem[dado] = 1
-
-    for quantidade in contagem.values():
-        if quantidade >= 5:
-            return 50
-    return 0
